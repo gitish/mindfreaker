a22080190592db202662aa70fddc52284dc3d18f
shaiendra <sshail@sapient.com>
2016-07-14 12:34:18 +0100

    correcting configuration issue

--- modules/admin/admin.js
+++ modules/admin/admin.js
@@ -6,12 +6,30 @@ var dm=require('../../model/dataModel.js');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
+var result=0;
 exports.main=function(req,res){
 	try{
-		var file=Math.floor((Math.random() * 5) + 1);
-		res.sendfile("./public/games/"+file+".html");
+		load(req,res,showResult);
 	}catch(e){
+		console.log(e);
 		res.end("failed");
+	}
+}
+var showResult=function(req,res){
+	console.log("game count="+result);
+	var file=Math.floor((Math.random() * result) + 1);
+	console.log("Random file is : " + file +".html");
+	res.sendfile("./public/games/"+file+".html");
+}
+var load=function(req,res,fu){
+	try{
+		fs.readFile("./public/games/app.json", 'utf-8', function(err, data) {
+			console.log(data);
+			result=JSON.parse(data).count;
+			console.log(result);
+			fu(req,res);
+		});
+	}catch(e){
+		console.log("No data found");
 	}
 }
