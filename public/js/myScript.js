a1b34710dc4fdca77805ceff3d9a6d7620ed1765
shaiendra <sshail@sapient.com>
2016-07-14 22:39:28 +0100

    adding client javascript

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -14,6 +14,33 @@ $(function() {
 	$("#home").click(function(){
 		loadBody("/main");
 	});
+	$("#oldQ").click(function(){
+		loadBody("/oldQPage");
+	});
+	$('input[type="submit"]').attr('disabled', true);
+	$("textarea").on('keyup', function(){
+		$('input[type="submit"]').attr('disabled' , !($("#txtAnswer").val() != ''));
+	});
+	$("#btnSubmit").click(function () {
+		$.ajax({
+			url : "/submitAnswer",
+			type :"post",
+			data : {
+				"userName": $("#txtName").val(),
+				"ans": $("#txtAnswer").val()
+			},
+			success : function(result) {
+				$("#dvPanel1").html("<h4>"+result+"</h4>");
+				resetAll();
+			}
+		});
+	});
+
+	var resetAll=function(){
+		$("#txtName").val('');
+		$("#txtAnswer").val('')
+		$("#btnSubmit").attr('disabled',true);
+	};
 	loadBody("/main");
 });
 
